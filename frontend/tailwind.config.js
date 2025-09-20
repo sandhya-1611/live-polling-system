25c563356bcdfbf4861ce71bb72bfb3329d272ea
sandhya-1611 <141938427+sandhya-1611@users.noreply.github.com>
2025-09-20 14:31:28 +0530

    added frontend initials

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7765DA',
+        secondary: '#5767D0',
+        accent: '#4F0DCE',
+        gray: {
+          light: '#F2F2F2',
+          dark: '#373737',
+          medium: '#6E6E6E',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
